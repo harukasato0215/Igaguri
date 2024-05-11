70edba497ff0a458d41ca5a9656ec4a32e548638
harukasato0215 <popo02150125@gmail.com>
2024-05-11 18:33:30 +0900

    完成

--- Assets/IgaguriController.cs
+++ Assets/IgaguriController.cs
@@ -14,12 +14,13 @@ public class IgaguriController : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
     }
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
-        Shoot(new Vector3(0, 200, 2000));
+        // Shoot(new Vector3(0, 200, 2000));
     }
 
     // Update is called once per frame
